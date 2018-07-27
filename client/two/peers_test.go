3b4dda341ff04c184c44ec86979c11035e418de3
Chris Johnson <chrisjohnsonmail@gmail.com>
2018-07-27 16:21:30 -0500

    Update peers_test.go

--- client/two/peers_test.go
+++ client/two/peers_test.go
@@ -145,6 +145,10 @@ func TestPeersService_GetBadResponse(t *testing.T) {
 	responseStruct, response, err := client.Peers.Get(context.Background(), "1.2.3.4")
 	testGeneralExpectError(t, "Peers.Get", err)
 	testResponseUrl(t, "Peers.Get", response, "/api/peers/1.2.3.4")
+	if response.StatusCode != http.StatusInternalServerError {
+		t.Error("StatusCode should be 500")
+	}
+	
 	if responseStruct != nil {
 		t.Error("responseStruct should be nil")
 	}
